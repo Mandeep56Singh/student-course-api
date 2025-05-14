b52c0d3cdde8a2dd9395eac3b52326269f5da311
mandeepSingh <mandeepraj2312@gmail.com>
2025-05-14 16:10:57 +0530

    refactor: make description fields required in validation schema

--- src/validator/courseValidation.schema.ts
+++ src/validator/courseValidation.schema.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const courseValidationSchema = z.object({
   body: z.object({
     title: z.string().min(1, "Title is required"),
-    description: z.string().optional(),
+    description: z.string().min(20, "Description must be 20 digits long"),
     credits: z.number().min(1, "credits are required"),
   }),
 });
@@ -22,6 +22,11 @@ export const courseUpdateValidationSchema = courseIdValidationSchema.merge(
     body: courseValidationSchema
   })
 )
-
+export const courseSearchValidationSchema = z.object({
+  query: z.object({
+     title: z.string().min(1, "Title is required"),
+  })
+})
+export type CourseSearchQueryType = z.infer<typeof courseSearchValidationSchema>["query"]
 export type CourseId = z.infer<typeof courseIdValidationSchema>["params"]
 export type CourseType = z.infer<typeof courseValidationSchema>["body"];
