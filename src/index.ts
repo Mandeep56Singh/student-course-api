7476c52f4f2d5b5b9073645bffd66680ac51810b
mandeepSingh <mandeepraj2312@gmail.com>
2025-05-14 18:38:05 +0530

    feat: add logging using morgan

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { authMiddleware } from "./middlewares/authMiddleware.js";
 import { authRouter } from "./routes/auth.routes.js";
 import { courseRouter } from "./routes/course.routes.js";
 import { studentRouter } from "./routes/student.routes.js";
+import morgan from 'morgan'
 const PORT = process.env.PORT || 300;
 
 const app = express();
@@ -12,6 +13,7 @@ const app = express();
 // middlewares
 app.use(express.json());
 app.use(cookieParser());
+app.use(morgan("dev")); 
 // routes
 app.use("/auth", authRouter);
 app.use("/courses", authMiddleware, courseRouter);
